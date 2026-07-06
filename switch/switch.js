6f5fc065074b8623c28d01abb7c5f0e8c748647d
eranneofvulcan <erannelenz@gmail.com>
2026-07-05 22:26:19 -0500

    Add files via upload

---
+++ switch/switch.js
@@ -0,0 +1,31 @@
+let favMonth = prompt("What is your favorite month?");
+favMonth = favMonth.toLowerCase();
+
+switch (favMonth) {
+  case "march":
+  case "april":
+  case "may":
+    console.log("Spring is nice with everything blooming.");
+    break;
+
+  case "june":
+  case "july":
+  case "august":
+    console.log("You enjoy the summer months!");
+    break;
+
+  case "september":
+  case "october":
+  case "november":
+    console.log("Fall is fun with all of the pretty colors.");
+    break;
+
+  case "december":
+  case "january":
+  case "february":
+    console.log("You love the winter months!");
+    break;
+
+  default:
+    console.log("Other months are interesting too!");
+}
